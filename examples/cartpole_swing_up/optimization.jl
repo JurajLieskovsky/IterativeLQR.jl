47a2e4cc384cf596e4a76ea00a3c994f396b1e47
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-11-02 20:28:42 +0100

    fmt

--- examples/cartpole_swing_up/optimization.jl
+++ examples/cartpole_swing_up/optimization.jl
@@ -12,7 +12,7 @@ using Plots
 # Horizon and timestep
 T = 2
 N = 100
-h = T/N
+h = T / N
 
 # Dynamics
 model = CartPole.Model(9.8, 1, 0.5, 0.1)
@@ -45,7 +45,7 @@ function running_cost_diff!(dLdx, dLdu, ddLdxx, ddLdxu, ddLduu, x, u)
 end
 
 # Final cost
-final_cost(x) = 1 + cos(x[2]) + 1e-1 * x[1]^2 + 1e0*x[3]^2 + 1e0*x[4]^2
+final_cost(x) = 1 + cos(x[2]) + 1e-1 * x[1]^2 + 1e0 * x[3]^2 + 1e0 * x[4]^2
 
 function final_cost_diff!(dΦdx, ddΦdxx, x)
     result = DiffResults.HessianResult(x)
@@ -62,6 +62,7 @@ workset = IterativeLQR.Workset{Float64}(4, 1, N)
 
 IterativeLQR.set_initial_state!(workset, [0.0, 0, 0, 0])
 IterativeLQR.set_initial_inputs!(workset, [[1e-3] for _ in 1:N])
+# IterativeLQR.set_initial_inputs!(workset, [[1e-4 * 2 * pi * k / N] for k in 0:N-1])
 
 # Plotting callback
 function plotting_callback(workset)
@@ -70,7 +71,7 @@ function plotting_callback(workset)
     position_plot = plot()
     for i = 1:2
         state_series = [x[i] for x in nominal_trajectory(workset).x]
-        plot!(position_plot, range, state_series, label="x"*string(i))
+        plot!(position_plot, range, state_series, label="x" * string(i))
     end
 
     input_plot = plot()
@@ -80,7 +81,7 @@ function plotting_callback(workset)
     cost_plot = plot()
     plot!(cost_plot, range, cumsum(nominal_trajectory(workset).l), label="c", seriestype=:steppost)
 
-    plt = plot(position_plot, input_plot, cost_plot, layout=(3,1))
+    plt = plot(position_plot, input_plot, cost_plot, layout=(3, 1))
     display(plt)
 
     return plt
