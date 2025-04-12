4407a65adb6c148848190ed086b3431516d4da1a
Juraj Lieskovsky <lieskovsky.juraj@gmail.com>
2025-04-12 14:23:26 +0200

    indicator -> projection

--- src/constraints.jl
+++ src/constraints.jl
@@ -2,11 +2,11 @@
 mutable struct Constraints{T}
     ρ::T
 
-    z_indicator::Union{Function,Nothing}
+    z_projection::Union{Function,Nothing}
     z::Vector{T}
     α::Vector{T}
 
-    w_indicator::Union{Function,Nothing}
+    w_projection::Union{Function,Nothing}
     w::Vector{Vector{T}}
     β::Vector{Vector{T}}
 
@@ -22,12 +22,12 @@ end
 
 ## set functions
 
-function set_terminal_constraint_indicator_function!(workset, fun)
-    workset.constraints.z_indicator = fun
+function set_terminal_constraint_projection_function!(workset, fun)
+    workset.constraints.z_projection = fun
 end
 
-function set_input_constraint_indicator_function!(workset, fun)
-    workset.constraints.w_indicator = fun
+function set_input_constraint_projection_function!(workset, fun)
+    workset.constraints.w_projection = fun
 end
 
 function set_penalty_parameter!(workset, ρ_new)
@@ -61,14 +61,14 @@ function add_penalty_derivatives!(workset)
     @unpack vx, vxx = workset.value_function
 
     @unpack ρ = workset.constraints
-    @unpack z_indicator, z, α = workset.constraints
-    @unpack w_indicator, w, β = workset.constraints
+    @unpack z_projection, z, α = workset.constraints
+    @unpack w_projection, w, β = workset.constraints
 
-    if z_indicator !== nothing
+    if z_projection !== nothing
         add_penalty_derivative!(vx[N+1], vxx[N+1], ρ, x[N+1] - z + α)
     end
 
-    if w_indicator !== nothing
+    if w_projection !== nothing
         for k in 1:N
             add_penalty_derivative!(lu[k], luu[k], ρ, u[k] - w[k] + β[k])
         end
@@ -80,12 +80,12 @@ end
 function evaluate_penalties(workset, trajectory)
     @unpack N = workset
     @unpack ρ = workset.constraints
-    @unpack z_indicator, z, α = workset.constraints
-    @unpack w_indicator, w, β = workset.constraints
+    @unpack z_projection, z, α = workset.constraints
+    @unpack w_projection, w, β = workset.constraints
 
-    penalty = (z_indicator !== nothing) ? evaluate_penalty(ρ, trajectory.x[N+1] - z + α) : 0
+    penalty = (z_projection !== nothing) ? evaluate_penalty(ρ, trajectory.x[N+1] - z + α) : 0
 
-    if (w_indicator !== nothing)
+    if (w_projection !== nothing)
         for k in 1:N
             penalty += evaluate_penalty(ρ, trajectory.u[k] - w[k] + β[k])
         end
@@ -96,30 +96,30 @@ end
 
 function update_slack_variables!(workset, trajectory)
     @unpack N = workset
-    @unpack z_indicator, z, α = workset.constraints
-    @unpack w_indicator, w, β = workset.constraints
+    @unpack z_projection, z, α = workset.constraints
+    @unpack w_projection, w, β = workset.constraints
 
-    if z_indicator !== nothing
-        z .= z_indicator(trajectory.x[N+1] + α)
+    if z_projection !== nothing
+        z .= z_projection(trajectory.x[N+1] + α)
     end
 
-    if w_indicator !== nothing
+    if w_projection !== nothing
         for k in 1:N
-            w[k] .= w_indicator(trajectory.u[k] + β[k])
+            w[k] .= w_projection(trajectory.u[k] + β[k])
         end
     end
 end
 
 function update_dual_variables!(workset, trajectory)
     @unpack N = workset
-    @unpack z_indicator, z, α = workset.constraints
-    @unpack w_indicator, w, β = workset.constraints
+    @unpack z_projection, z, α = workset.constraints
+    @unpack w_projection, w, β = workset.constraints
 
-    if z_indicator !== nothing
+    if z_projection !== nothing
         α .= α + trajectory.x[N+1] - z
     end
 
-    if w_indicator !== nothing
+    if w_projection !== nothing
         for k in 1:N
             β[k] .= β[k] + trajectory.u[k] - w[k]
         end
