a25dc9297443be576b4e9b1d87d93e9ec8f3b230
Juraj Lieskovsky <lieskovsky.juraj@gmail.com>
2025-04-12 12:19:29 +0200

    constraints as mutable struct

--- src/constraints.jl
+++ src/constraints.jl
@@ -1,13 +1,20 @@
 # Terminal state constraint
-struct Constraints{T}
-    ρ::Ref{T}
-    _xN::Ref{Bool}
+mutable struct Constraints{T}
+    ρ::T
+
+    _xN::Bool
     αN::Vector{T}
     zN::Vector{T}
+
+    # w::Vector{Vector{T}}
+    # β::Vector{Vector{T}}
 
     function Constraints{T}(nx) where {T} # , nu, N) where {T}
         αN = zeros(T, nx)
         zN = zeros(T, nx)
+        # β = [zeros(T, nu) for _ in 1:N]
+        # w = [zeros(T, nu) for _ in 1:N]
+
         new(zero(T), true, αN, zN)#, β, w)
     end
 end
@@ -17,8 +24,8 @@ end
 function set_penalty_parameter!(workset, ρ_new)
     @unpack ρ, αN = workset.constraints
 
-    ratio = ρ[] / ρ_new
-    ρ[] = ρ_new
+    ratio = ρ / ρ_new
+    ρ = ρ_new
 
     αN .*= ratio
 
@@ -40,8 +47,8 @@ function add_penalty_derivatives!(workset)
     @unpack vx, vxx = workset.value_function
     @unpack ρ, _xN, αN, zN = workset.constraints
 
-    if _xN[]
-        add_penalty_derivative!(vx[N+1], vxx[N+1], ρ[], x[N+1] - zN + αN)
+    if _xN
+        add_penalty_derivative!(vx[N+1], vxx[N+1], ρ, x[N+1] - zN + αN)
     end
 
     return nothing
@@ -51,7 +58,7 @@ function evaluate_penalties(workset, trajectory)
     @unpack N = workset
     @unpack ρ, _xN, zN, αN = workset.constraints
 
-    return _xN[] ? evaluate_penalty(ρ[], trajectory.x[N+1] - zN + αN) : 0
+    return _xN ? evaluate_penalty(ρ, trajectory.x[N+1] - zN + αN) : 0
 end
 
 function update_slack_variables!(workset, trajectory, terminal_constraint)
@@ -59,10 +66,10 @@ function update_slack_variables!(workset, trajectory, terminal_constraint)
     @unpack _xN, zN = workset.constraints
 
     if terminal_constraint !== nothing
-        _xN[] = true
+        _xN = true
         zN .= terminal_constraint(trajectory.x[N+1])
     else
-        _xN[] = false
+        _xN = false
     end
 end
 
@@ -70,7 +77,7 @@ function update_dual_variables!(workset, trajectory)
     @unpack N = workset
     @unpack _xN, zN, αN = workset.constraints
 
-    if _xN[]
+    if _xN
         αN .= αN + trajectory.x[N+1] - zN
     end
 
