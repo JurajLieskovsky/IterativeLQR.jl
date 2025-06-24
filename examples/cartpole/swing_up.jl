9664787cb62756cb15f64eecee2c21f9e754f03a
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-06-24 22:44:28 +0200

    really nice convergence with current settings

--- examples/cartpole/swing_up.jl
+++ examples/cartpole/swing_up.jl
@@ -108,17 +108,17 @@ IterativeLQR.set_initial_state!(workset, x₀)
 IterativeLQR.set_initial_inputs!(workset, us₀)
 
 IterativeLQR.set_terminal_state_projection_function!(workset, terminal_state_projection)
-IterativeLQR.set_terminal_state_constraint_parameter!(workset, [0, 1e1, 1e1, 1e1])
+IterativeLQR.set_terminal_state_constraint_parameter!(workset, [1e1, 1e1, 1e1, 1e1])
 
 IterativeLQR.set_input_projection_function!(workset, input_projection)
-IterativeLQR.set_input_constraint_parameter!(workset, 1e-1)
+IterativeLQR.set_input_constraint_parameter!(workset, 1e-2)
 
 IterativeLQR.set_state_projection_function!(workset, state_projection)
-IterativeLQR.set_state_constraint_parameter!(workset, [1e-0, 0, 0, 0])
+IterativeLQR.set_state_constraint_parameter!(workset, [1e-4, 1e-4, 1e-4, 1e-4])
 
 df = IterativeLQR.iLQR!(
     workset, dynamics!, dynamics_diff!, running_cost, running_cost_diff!, final_cost, final_cost_diff!,
-    stacked_derivatives=true, regularization=:min, maxouter=100,
+    stacked_derivatives=true, regularization=:min, maxouter=200,
     verbose=true, logging=true, plotting_callback=plotting_callback
 )
 
