8a18e750d630cc7567f4569a22b927cf102caf90
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-08-18 15:35:51 +0200

    infinite horizon final cost

--- examples/quadrotor/hover_to_hover.jl
+++ examples/quadrotor/hover_to_hover.jl
@@ -11,13 +11,14 @@ using Plots
 using DataFrames, CSV
 using BenchmarkTools
 using Infiltrator
+using MatrixEquations
 
 # Quadrotor model
 quadrotor = QuadrotorODE.System(9.81, 0.5, diagm([0.0023, 0.0023, 0.004]), 0.1750, 1.0, 0.0245)
 
 # Horizon and timestep
-T = 4
-N = 400
+T = 3
+N = 300
 h = T / N
 
 # Target state
@@ -78,27 +79,26 @@ function running_cost_diff!(lx, lu, lxx, lxu, luu, x, u, k)
 end
 
 # Final cost
-S = [
-    769.104 5.28962e-9 -9.77477e-10 -3.42266e-9 715.141 -2.39279e-9 241.915 1.89084e-9 -5.83008e-11 -1.93777e-11 4.34069 -1.06051e-10
-    5.28962e-9 769.104 -4.98199e-11 -715.141 5.29192e-9 -3.27945e-9 2.28355e-9 241.915 1.73479e-11 -4.34069 3.07872e-11 -1.3701e-10
-    -9.77477e-10 -4.98199e-11 518.129 -7.07492e-10 -1.20579e-9 4.63535e-10 -3.66676e-10 4.70971e-11 81.6384 -4.5972e-12 -7.04069e-12 2.5152e-11
-    -3.42266e-9 -715.141 -7.07492e-10 3160.22 -1.36165e-8 5.338e-9 -2.71134e-9 -534.185 -1.62892e-10 19.956 -8.46541e-11 2.21343e-10
-    715.141 5.29192e-9 -1.20579e-9 -1.36165e-8 3160.22 -6.48328e-10 534.185 2.74521e-9 -1.89806e-10 -8.42911e-11 19.956 -5.13592e-11
-    -2.39279e-9 -3.27945e-9 4.63535e-10 5.338e-9 -6.48328e-10 719.578 -9.02046e-10 -1.4253e-9 9.44497e-11 3.30704e-11 -2.60711e-12 27.6493
-    241.915 2.28355e-9 -3.66676e-10 -2.71134e-9 534.185 -9.02046e-10 147.189 9.57854e-10 -3.55125e-11 -1.61548e-11 3.29503 -4.24648e-11
-    1.89084e-9 241.915 4.70971e-11 -534.185 2.74521e-9 -1.4253e-9 9.57854e-10 147.189 1.72244e-11 -3.29503 1.63675e-11 -5.96181e-11
-    -5.83008e-11 1.73479e-11 81.6384 -1.62892e-10 -1.89806e-10 9.44497e-11 -3.55125e-11 1.72244e-11 41.4829 -1.04616e-12 -1.14007e-12 4.78914e-12
-    -1.93777e-11 -4.34069 -4.5972e-12 19.956 -8.42911e-11 3.30704e-11 -1.61548e-11 -3.29503 -1.04616e-12 1.45232 -5.25134e-13 1.37353e-12
-    4.34069 3.07872e-11 -7.04069e-12 -8.46541e-11 19.956 -2.60711e-12 3.29503 1.63675e-11 -1.14007e-12 -5.25134e-13 1.45232 -2.59839e-13
-    -1.06051e-10 -1.3701e-10 2.5152e-11 2.21343e-10 -5.13592e-11 27.6493 -4.24648e-11 -5.96181e-11 4.78914e-12 1.37353e-12 -2.59839e-13 9.80969
-]
+## Taylor expansions of the system's dynamics and running cost around equilibrium
+A, B = zeros(12,12), zeros(12,4)
+lx_eq, lu_eq, Q, lxu_eq, R = zeros(12), zeros(4), zeros(12,12), zeros(12,4), zeros(4,4)
 
+dynamics_diff!(A, B, xₜ, uₜ, 0)
+running_cost_diff!(lx_eq, lu_eq, Q, lxu_eq, R, xₜ, uₜ, 0)
+
+## check that running cost has a convex approximation and local minimum at equilibrium
+@assert isposdef(R)
+@assert isposdef(Q) # more stict than necessary
+@assert all(map(der -> all(isapprox.(der, 0)), (lx_eq, lu_eq, lxu_eq)))
+
+## value function's matrix
+S, _ = ared(A, B, R, Q)
+
+## resulting final cost
 function final_cost(x, _)
     dx = QuadrotorODE.state_difference(x, xₜ)
     return dx' * S * dx
 end
-
-final_cost(_, _) = 0
 
 function final_cost_diff!(Φx, Φxx, x, k)
     E = QuadrotorODE.jacobian(x)
