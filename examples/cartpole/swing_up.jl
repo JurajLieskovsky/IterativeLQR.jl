414c8e2c4a6aa45fa6f2ab7954335758b9ddfdd4
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-09-05 16:29:56 +0200

    benchmark option in cartpole swing-up

--- examples/cartpole/swing_up.jl
+++ examples/cartpole/swing_up.jl
@@ -142,6 +142,20 @@ df = IterativeLQR.iLQR!(
     verbose=true, logging=true, plotting_callback=plotting_callback
 )
 
+## Benchmarking
+benchmark_iter = findfirst(J -> (J-df.J[end]) <= 1e-3 * df.J[end], df.J)
+benchmark_res = @benchmark begin
+    IterativeLQR.set_initial_inputs!(workset, [u₀(k) for k in 1:N])
+    IterativeLQR.iLQR!(
+        workset, dynamics!, dynamics_diff!, running_cost, running_cost_diff!, final_cost, final_cost_diff!,
+        stacked_derivatives=true, algorithm=algorithm,
+        regularization=regularization, regularization_approach=regularization_approach,
+        verbose=false, logging=false,
+        maxiter=benchmark_iter
+    )
+end
+display(benchmark_res)
+
 # Save iterations log to csv
 regularization_string = mapreduce(a -> "-$a", *, regularization)
 CSV.write("cartpole/results/cartpole-$algorithm$regularization_string-$regularization_approach.csv", df)
