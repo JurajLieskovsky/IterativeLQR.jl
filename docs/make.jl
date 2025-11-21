9847cf67baf9e35efac0a1f5bc889204449bfa0f
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-11-21 11:04:56 +0100

    push

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,28 @@
 using Documenter, IterativeLQR
+using Literate
+
+SOURCE = joinpath(@__DIR__, "..", "examples", "cartpole", "swing_up.jl")
+OUTPUT = joinpath(@__DIR__, "src", "generated")
+
+Literate.markdown(SOURCE, OUTPUT, codefence="```julia" => "```")
+Literate.script(SOURCE, OUTPUT)
 
 makedocs(
     sitename="IterativeLQR.jl",
     pages=[
         "Home" => "index.md",
         "Getting started" => "guide.md",
+        "Examples" => [
+            "Cartpole swing-up" => "generated/swing_up.md"
+        ],
         "reference.md"
     ],
-    repo = "github.com/JurajLieskovsky/IterativeLQR.jl.git"
+    repo = Remotes.GitHub("JurajLieskovsky", "IterativeLQR.jl")
+    # repo="github.com/JurajLieskovsky/IterativeLQR.jl.git"
 )
 
 
 deploydocs(
-    repo = "github.com/JurajLieskovsky/IterativeLQR.jl.git"
+    repo = Remotes.GitHub("JurajLieskovsky", "IterativeLQR.jl")
+    # repo="github.com/JurajLieskovsky/IterativeLQR.jl.git"
 )
